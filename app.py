511e49031fc8235de9bb2ec29dcacaf805e3bd05
Juan S. <jsilvoso@hotmail.com>
2025-05-10 16:36:10 +0200

    Añadiendo descargar .csv de  métricas

--- app.py
+++ app.py
@@ -192,6 +192,7 @@ def descargar_metricas():
             writer.writeheader()
     return send_file(archivo, as_attachment=True)
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
